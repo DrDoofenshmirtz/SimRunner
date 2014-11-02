17047e24a83eaad08097f7802a9d4b62c4902d46
DrDoofenshmirtz <frank.mosebach@gmx.de>
2014-11-02 20:06:43 +0100

    Implemented basic (sub-) process execution functions.

--- src/fm/simrunner/exec.clj
+++ src/fm/simrunner/exec.clj
@@ -3,9 +3,30 @@
   
   "Utilities for the Execution of Processes."
   
-    :author "Frank Mosebach"})
+    :author "Frank Mosebach"}
+  (:require
+    [clojure.java.io :as jio])
+  (:import 
+    (java.io InputStream PrintWriter)))
 
-(defn exec
-  "TODO: spawn a process, drain its ouput, wait for termination..."
-  [])
+(defn write-lines [^InputStream source ^PrintWriter target]
+  (with-open [reader (jio/reader source)]
+    (doseq [line (line-seq reader)]
+      (.println target line))))
+
+(defn out>-system-out [out]
+  (write-lines out *out*))
+
+(defn err>-system-err [err]
+  (write-lines err *err*))
+
+(defn exec [path & args]
+  (.start (ProcessBuilder. (into-array (cons path args)))))
+
+(defn drain-outputs 
+  [process & {:keys [out> err>] 
+              :or {out> out>-system-out err> err>-system-err}}]
+  (future (out> (.getInputStream process)))
+  (future (err> (.getErrorStream process)))
+  nil)
 
