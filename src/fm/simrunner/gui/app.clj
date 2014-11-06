903ff5828db2115707a2dd817f24bb1b0941862f
DrDoofenshmirtz <frank.mosebach@gmx.de>
2014-11-06 08:56:25 +0100

    Create an app to be used as execution context for event processing.

--- src/fm/simrunner/gui/app.clj
+++ src/fm/simrunner/gui/app.clj
@@ -7,38 +7,43 @@
   (:require
     [fm.simrunner.gui (core :as gui) 
                       (view :as view) 
-                      (wiring :as wiring)])
-  (:import 
-    (java.awt BorderLayout)
-    (javax.swing SwingUtilities 
-                 JOptionPane)))
+                      (wiring :as wiring)]))
 
 (defmulti on-action {:private true} (fn [action & _] action))
 
-(defmethod on-action :open-config [_ model & args]
-  (println (format "open-config{model: %s args: %s}" model args))
+(defmethod on-action :open-config [_ app & args]
+  (println (format "open-config{args: %s}" args))
   (gui/choose-file (-> args first :widget) :title "Open Config"))  
 
-(defmethod on-action :default [action model & args]
-  (println (format "on-action{action: %s model: %s args: %s}" 
-                   action model args)))
+(defmethod on-action :default [action app & args]
+  (println (format "on-action{action: %s args: %s}" action args)))
 
 (defmulti on-event {:private true} (fn [_ event-id & _] event-id))
 
-(defmethod on-event :action-performed [model _ & args]
-  (on-action (-> args first meta :action) model args))
+(defmethod on-event :action-performed [app _ & args]
+  (on-action (-> args first meta :action) app args))
 
-(defmethod on-event :default [model event-id & args]
-  (println (format "on-event{model: %s id: %s args: %s}" model event-id args)))
+(defmethod on-event :default [app event-id & args]
+  (println (format "on-event{id: %s args: %s}" event-id args)))
+
+(defn- app [config view]
+  {:config config
+   :worker (agent nil)
+   :state  (atom {:ui {:view      view
+                       :updating? false}})})
+
+(defn- event-handler [app]
+  (fn [& args]
+    (when-not (-> app :state deref :ui :updating?)
+      (apply on-event app args))))
 
 (defn start [config]
   @(gui/gui-do
     (let [frame (view/simrunner-frame)
           view  (-> frame :contents :simrunner-view)
           frame (:widget frame)
-          model (ref {:app-config config
-                      :worker     (agent nil)})]
-      (wiring/wire-up view (partial on-event model))
+          app   (app config view)]
+      (wiring/wire-up view (event-handler app))
       (.setTitle frame "SimRunner (c) 2014 DEINC")
       (.show frame))))
 
