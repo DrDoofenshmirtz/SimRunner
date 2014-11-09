398fb9583b5949548116e72c56893c1e89b9bfdc
DrDoofenshmirtz <frank.mosebach@gmx.de>
2014-11-09 20:36:34 +0100

    Extracted action specs, consider enabled actions when unlocking.

--- src/fm/simrunner/gui/view.clj
+++ src/fm/simrunner/gui/view.clj
@@ -51,19 +51,35 @@
                                         {:type       :text
                                          :label-text "ACSR Size"}]))
 
+(def ^{:private true} action-ids [:open-config
+                                  :save-config
+                                  :save-config-as
+                                  :run-simulation
+                                  :stuetz-count])
+
+(def ^{:private true} action-specs 
+                      (map #(assoc %2 :action %1)
+                           action-ids
+                           [{:text         "Open" 
+                             :tooltip-text "Open Config File"}
+                            {:text         "Save" 
+                             :tooltip-text "Save current Config File"}
+                            {:text         "Save As" 
+                             :tooltip-text "Save Config in new File"}
+                            {:text         "Run" 
+                             :tooltip-text "Start Simulation Run"}]))
+
+(defn- make-buttons []
+  (->> action-specs
+       (map #(vector (:action %) (apply gui/button (apply concat %))))
+       (into {})))                      
+                      
 (defn- tool-bar []
-  (let [open-button    (gui/button :action :open-config
-                                   :text "Open" 
-                                   :tooltip-text "Open Config File") 
-        save-button    (gui/button :action :save-config
-                                   :text "Save" 
-                                   :tooltip-text "Save current Config File")
-        save-as-button (gui/button :action :save-config-as
-                                   :text "Save As" 
-                                   :tooltip-text "Save Config in new File")
-        run-button     (gui/button :action :run-simulation
-                                   :text "Run" 
-                                   :tooltip-text "Start Simulation Run")
+  (let [buttons        (make-buttons)
+        open-button    (:open-config buttons) 
+        save-button    (:save-config buttons)
+        save-as-button (:save-config-as buttons)
+        run-button     (:run-simulation buttons)
         tool-bar       (doto (JToolBar.)
                              (.setFloatable false)
                              (.add (:widget open-button))
@@ -163,6 +179,11 @@
     (filter (comp input-ids :id meta)
             (widget-seq view))))
 
+(defn- buttons [view]
+  (let [action-ids (into #{} action-ids)]
+    (filter (comp action-ids :action meta)
+            (widget-seq view))))
+
 (defmulti set-value {:private true} (fn [input value] 
                                       [(type input) (type value)]))
 
@@ -198,7 +219,7 @@
 (defmethod set-value :default [input value]
   (println (format "set-value{%s value: %s}" (type input) value)))
 
-(defn- update-inputs [view values]
+(defn- update-inputs [view {values :input-values}]
   (doseq [input (inputs view)]
     (let [input-id (:id (meta input))
           value    (input-id values)]
@@ -206,15 +227,27 @@
   view)
 
 (defn render [view model]
-  (update-inputs view (:input-values model)))
+  (update-inputs view model))
 
 (defn lock [view model]
   (doseq [{widget :widget} (widget-seq view)]
     (.setEnabled widget false))
   view)
 
-(defn unlock [view model]
-  (doseq [{widget :widget} (widget-seq view)]
+(defn- unlock-buttons [view model]
+  (let [{:keys [enabled-actions]} model
+        buttons (filter (comp enabled-actions :action meta) (buttons view))]
+    (doseq [{widget :widget} buttons]
+      (.setEnabled widget true))
+    view))
+
+(defn- unlock-inputs [view model]
+  (doseq [{widget :widget} (mapcat widget-seq (inputs view))]
     (.setEnabled widget true))
   view)
+
+(defn unlock [view model]
+  (-> view
+      (unlock-buttons model)
+      (unlock-inputs model)))
 
