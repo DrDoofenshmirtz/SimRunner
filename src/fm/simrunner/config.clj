74720779f757244f0c9548dfd239101b2357c83d
DrDoofenshmirtz <frank.mosebach@gmx.de>
2014-11-13 18:51:13 +0100

    Create a config with validated string values.
    Create valid string representations of the respective parameter values.

--- src/fm/simrunner/config.clj
+++ src/fm/simrunner/config.clj
@@ -4,69 +4,47 @@
   (:import 
     (java.io File)))
 
-(defn- string->file [^String string]
-  (let [string (-> string str .trim)]
-    (when-not (.isEmpty string)
-      (File. string))))
+(defn- value->string [value]
+  (let [value (-> value str .trim)]
+    (when-not (.isEmpty value)
+      value)))
 
-(defn- file? [value]
-  (instance? File value))
+(defn- file-value [value]
+  (value->string value))
 
-(defn- string->double [^String string]
-  (try
-    (Double/valueOf (str string))
-    (catch NumberFormatException x)))
+(defn- number-value [value]
+  (when-let [value (value->string value)]
+    (try
+      (let [number (Double/valueOf value)]
+        (when-not (or (Double/isNaN number) (Double/isInfinite number))
+          value))
+      (catch NumberFormatException x))))
 
-(defn- string->boolean [^String string]
-  (let [string (-> string str .trim)]
-    (cond
-      (= "1" string) true
-      (= "0" string) false
-      :else nil)))
+(defn- boolean-value [value]
+  (#{"0" "1"} (value->string value)))
 
-(defn- boolean? [value]
-  (instance? Boolean value))
+(def param-ids [:input-file 
+                :eps 
+                :log-num 
+                :stuetz-count 
+                :correction 
+                :max-deviation 
+                :calc-err 
+                :acsr-size])
 
-(def ^{:private true} parameter-ids [:input-file 
-                                     :eps 
-                                     :log-num 
-                                     :stuetz-count 
-                                     :correction 
-                                     :max-deviation 
-                                     :calc-err 
-                                     :acsr-size])
+(def ^{:private true} param-specs [{:value file-value}
+                                   {:value number-value}
+                                   {:value number-value}
+                                   {:value number-value}
+                                   {:value number-value}
+                                   {:value number-value}
+                                   {:value boolean-value}
+                                   {:value number-value}])
 
-(def ^{:private true} parameter-specs [{:valid?        file?
-                                        :string->value string->file}
-                                       {:valid?        number?
-                                        :string->value string->double}
-                                       {:valid?        number?
-                                        :string->value string->double}
-                                       {:valid?        number?
-                                        :string->value string->double}
-                                       {:valid?        number?
-                                        :string->value string->double}
-                                       {:valid?        number?
-                                        :string->value string->double}
-                                       {:valid?        boolean?
-                                        :string->value string->boolean}
-                                       {:valid?        number?
-                                        :string->value string->double}])
+(def ^{:private true} param-defs (zipmap param-ids param-specs))
 
-(def ^{:private true} parameter-defs (zipmap parameter-ids parameter-specs))
-     
-(defn- throw-undefined-param-id [id]
-  (throw (IllegalArgumentException. (format "Undefined parameter id: %s!" id))))
-
-(defn string->value [param-id value-string]
-  (if-let [string->value (get-in parameter-defs [param-id :string->value])]
-    (string->value value-string)
-    (throw-undefined-param-id param-id)))
-
-(defn value-valid? [param-id value]
-  (if-let [valid? (get-in parameter-defs [param-id :valid?])]
-    (valid? value)
-    (throw-undefined-param-id param-id)))
+(defn- value [param-id value]
+  ((get-in param-defs [param-id :value]) value))
 
 (defn- blank? [trimmed-line]
   (.isEmpty trimmed-line))
@@ -93,52 +71,20 @@
 (defn- trimmed [lines]
   (map #(-> % str .trim) lines))
 
-(defn strings->values [config]
-  (->> (merge (zipmap parameter-ids (repeat nil)) config)
-       (map (fn [[param-id value-string]]
-              [param-id (string->value param-id value-string)]))
-       (into {})))
-
-(defn validate [config]
-  (reduce (fn [validated param-id]
-            (let [value (param-id config)]
-              (if (value-valid? param-id value)
-                (-> validated
-                    (assoc param-id value)
-                    (vary-meta update-in [::invalid-ids] disj param-id))
-                (assoc validated param-id nil))))
-          (with-meta {}
-                     (assoc (meta config) 
-                            :type         ::config
-                            ::invalid-ids (into #{} parameter-ids)))
-          parameter-ids))
-
-(defn invalid-ids
-  ([config]
-    (invalid-ids config false))
-  ([config validate?]
-    (-> (if validate? (validate config) config) meta ::invalid-ids)))
-
-(defn config? [config]
-  (= ::config (type config)))
-
-(defn valid? 
-  ([config]
-    (valid? config false))
-  ([config validate?]
-    (let [config (if validate? (validate config) config)]
-      (and (config? config)
-           (-> config invalid-ids seq not)))))
+(defn config [map]
+  (reduce (fn [config param-id]
+            (if-let [value (value param-id (param-id map))]
+              (assoc config param-id value)
+              config))
+          {}
+          param-ids))
 
 (defn read-config-lines [lines]
   (let [values (-> lines
                    trimmed
                    skip-ignored
                    strip-comments)]
-    (-> parameter-ids
-        (zipmap values)
-        strings->values
-        validate)))
+    (-> param-ids (zipmap values) config)))
 
 (defn read-config-file [file]
   (with-open [reader (jio/reader file)]
