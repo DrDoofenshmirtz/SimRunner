761c89634366dce427a408616f8d316985e8a620
DrDoofenshmirtz <frank.mosebach@gmx.de>
2014-11-23 18:20:14 +0100

    Change to the working dir before launching the Java application.

--- dist/bin/SimRunner.sh
+++ dist/bin/SimRunner.sh
@@ -4,21 +4,23 @@
 #
 # This script expects to be located in the "bin" subdirectory of the
 # application's distribution. Consequently, the application's working
-# directory will point to this script's parent directory.
+# directory will point to the parent directory of "bin".
 
 WORKING_DIRECTORY=$0
 
-if [ -L $WORKING_DIRECTORY ]; 
+if [ -L "${WORKING_DIRECTORY}" ]; 
 then
   WORKING_DIRECTORY=`readlink $0`  
 fi
 
-WORKING_DIRECTORY=`dirname $WORKING_DIRECTORY`
-WORKING_DIRECTORY=`readlink -f $WORKING_DIRECTORY/..`
+WORKING_DIRECTORY=`dirname "${WORKING_DIRECTORY}"`
+WORKING_DIRECTORY=`readlink -f "${WORKING_DIRECTORY}"/..`
 
 echo "Starting SimRunner (c) 2014 DEINC..."
-echo "(Working directory: $WORKING_DIRECTORY)"
+echo "(Working directory: ${WORKING_DIRECTORY})"
 
-java -server -Xms64m -Xmx128m -cp "${WORKING_DIRECTORY}/lib/*" \
-fm.simrunner.Main "$WORKING_DIRECTORY"
+(cd "${WORKING_DIRECTORY}" && \
+java -server -Xms64m -Xmx128m \
+-cp "${WORKING_DIRECTORY}/lib/*" \
+fm.simrunner.Main "${WORKING_DIRECTORY}")
 
